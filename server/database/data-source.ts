c05791eea1e1ea4cd553effe295c596aaf255b1e
Gerasimov Daniil <danilger@gmail.com>
2025-09-05 09:42:05 +0500

    Добавлены начальные файлы проекта NestJS, включая конфигурацию TypeORM, настройки ESLint и Prettier, а также базовые контроллеры и сервисы. Созданы файлы для миграций и тестирования. Добавлен README с инструкциями по настройке и запуску проекта.

---
+++ server/database/data-source.ts
@@ -0,0 +1,25 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+
+// Загрузка переменных окружения из .env-файла
+dotenv.config();
+
+/**Экспорт конфигурации подключения к PostgreSQL для миграций TypeORM */
+export default new DataSource({
+  type: 'postgres', // Тип СУБД
+  host: process.env.HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: false, // Отключение автоматической синхронизации схемы
+  dropSchema: false, // Запрет на удаление схемы при запуске
+  logging: true, // Включение логгирования запросов
+  logger: 'advanced-console', // Тип логгера (если включено)
+  entities: [join(__dirname, '..', '**', '*.entity.{ts,js}')], // Пути к сущностям
+  migrations: [join(__dirname, '..', 'database', 'migrations', '**', '*.{ts,js}')], // Пути к миграциям
+  subscribers: [join(__dirname, '..', 'subscriber', '**', '*.{ts,js}')], // Пути к подписчикам событий
+  migrationsTableName: 'migrations', // Название таблицы для хранения миграций
+  migrationsRun: true
+});
---
+++ server/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: false, // Отключите в продакшене
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
