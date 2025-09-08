1892948a3f76927c686c7601613c52ab1f391fc5
Gerasimov Daniil <danilger@gmail.com>
2025-09-08 11:12:40 +0500

    добавил volume базы данных

--- server/database/data-source.ts
+++ server/database/data-source.ts
@@ -8,7 +8,7 @@ dotenv.config();
 /**Экспорт конфигурации подключения к PostgreSQL для миграций TypeORM */
 export default new DataSource({
   type: 'postgres', // Тип СУБД
-  host: process.env.HOST,
+  host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
